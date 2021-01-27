03f087673d36362581b8ec07e3ab70203b4670da
yangming <yangming@shinemo.com>
2021-01-27 16:20:08 +0800

    text husky

--- server/app/graphql/user/resolver.js
+++ server/app/graphql/user/resolver.js
@@ -1,6 +1,6 @@
 module.exports = {
   Query: {
-    user(root, { id }, ctx) {
+    user (root, { id }, ctx) {
       // return ctx.connector.user.fetchById(id);
       console.log(id)
       return Promise.resolve({ id: 1, name: '111' })
